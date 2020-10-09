cd96cb6d2b1e00e913a2613f2cfc64f349d04138
tau-upsilion <trinhty20@gmail.com>
2020-10-09 16:38:18 -0500

    testing react client vs angular

---
+++ client-react/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-primary">
+            <div className="row col-12 d-flex justify-content-center text-white"></div>
+            <span className="h3">Register</span>
+        </nav>
+    )
+}
+export default Header;
