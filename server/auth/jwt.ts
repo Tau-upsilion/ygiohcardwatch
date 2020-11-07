4c2ceb79cc54ea0198792bb59a7ab15d1f40efae
tau-upsilion <trinhty20@gmail.com>
2020-11-07 15:17:44 -0600

    update jwt, update dependencies

--- server/auth/jwt.ts
+++ server/auth/jwt.ts
@@ -10,9 +10,41 @@ export enum TokenType {
 type JWT = { exp: number; type: TokenType; sub: string};
 
 export const generateAccessToken = (userId: string) => {
-    return generateAccessToken(userId, TokenType.ACCESS_TOKEN);
+    return generateToken(userId, TokenType.ACCESS_TOKEN);
 };
 
 export const generateRefreshToken = (userId: string) => {
-    return generateAccessToken(userId, TokenType.REFRESH_TOKEN);
+    return generateToken(userId, TokenType.REFRESH_TOKEN);
+};
+
+const generateToken = (userId: string, type: TokenType) => {
+    const audience = config.get('');
+    const issuer = config.get('');
+    const secret = config.get('');
+    const expiresIn =
+        type === TokenType.ACCESS_TOKEN
+        ? config.get('authentication.token.expiresIn')
+        : config.get('authentication.refreshToken.expiresIn');
+    
+    const token = sign({ type }, secret, {
+        expiresIn,
+        audience: audience,
+        issuer: issuer,
+        subject: userId,
+    });
+
+    return {
+        token: encrypt(token),
+        expiration: (decode(token) as JWT).exp * 1000,
+    };
+}
+
+export const getTokenType = (token: string): TokenType => {
+    return (verify(token, config.get('authentication.token.secret')) as JWT).type;
+  };
+  
+export const parseTokenAndGetUserId = (token: string): string => {
+    const decryptedToken = decrypt(token);
+    const decoded = verify(decryptedToken, config.get('authentication.token.secret')) as JWT;
+    return decoded.sub || '';
 };
